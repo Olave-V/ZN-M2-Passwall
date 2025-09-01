16745d1c1e896bb80cfc9364b0547a121d771e03
Olave-V <76271929@qq.com>
2025-09-01 11:59:50 +0800

    Add files via upload

--- scripts/zn-m2/diy-part3.sh
+++ scripts/zn-m2/diy-part3.sh
@@ -15,32 +15,12 @@
 
 #修改版本信息
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='OpenWrt IPQ6000 ZN-M2 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
-
 # 替换golang版本为1.25
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # ttyd免登陆
-# sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
+#sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # design修改proxy链接
 sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
-
-# 删除指定的软件包及其相关依赖的源代码目录
-rm -rf feeds/luci/applications/luci-app-wireguard
-rm -rf feeds/luci/applications/luci-app-autoreboot
-rm -rf feeds/luci/applications/luci-app-lucky
-rm -rf feeds/luci/applications/luci-app-wol
-rm -rf feeds/luci/applications/luci-app-openvpn
-rm -rf feeds/luci/applications/luci-app-nps
-rm -rf feeds/luci/applications/luci-app-uhttpd
-rm -rf feeds/luci/applications/luci-app-ttyd
-
-# 额外删除一些相关的非LuCI软件包（可选，但更彻底）
-rm -rf feeds/packages/net/wireguard-tools
-rm -rf feeds/packages/net/openvpn
-rm -rf feeds/packages/utils/ttyd
-rm -rf feeds/packages/net/uhttpd
-rm -rf feeds/packages/net/wol
-rm -rf feeds/packages/net/lucky
-rm -rf feeds/packages/net/nps
